c6f60ce2449ba42e671eb4e03580f386a2c70f0c
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-09-30 16:51:59 +0100

    adds unbuffered channel to goroutine fn

--- cpu.go
+++ cpu.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	tensOfMillionsToCountTo := []int{9, 5, 11, 7}
 	runCpuHeavySync(tensOfMillionsToCountTo)
+	runCpuHeavyGoroutines(tensOfMillionsToCountTo)
 }
 
 func runCpuHeavySync(tensOfMillionsToCountTo []int) {
@@ -20,9 +21,29 @@ func runCpuHeavySync(tensOfMillionsToCountTo []int) {
 	fmt.Println("Synchronous function: time taken", watch.Milliseconds())
 }
 
+func runCpuHeavyGoroutines(tensOfMillionsToCountTo []int) {
+	watch := stopwatch.Start()
+	c := make(chan int)
+	for _, v := range tensOfMillionsToCountTo {
+		go countToNMillionGoroutine(v, c)
+		<-c
+	}
+	defer close(c)
+	watch.Stop()
+	fmt.Println("Goroutines function: time taken", watch.Milliseconds())
+}
+
 func countToNMillion(n int) {
 	count := 0
 	for i := 0; i < 1000000*n; i++ {
 		count += i
 	}
+}
+
+func countToNMillionGoroutine(n int, c chan int) {
+	count := 0
+	for i := 0; i < 1000000*n; i++ {
+		count += i
+	}
+	c <- count
 }
