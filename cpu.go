a528de2883c226afcee243cc73f00c58388cd772
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-09-30 15:28:58 +0100

    measures time taken for all numbers together

--- cpu.go
+++ cpu.go
@@ -8,19 +8,21 @@ import (
 
 func main() {
 	tensOfMillionsToCountTo := []int{9, 5, 11, 7}
-	m := make(map[int]string)
-	for _, v := range tensOfMillionsToCountTo {
-		countToNMillion(v, m)
-	}
-	fmt.Println(m)
+	runCpuHeavySync(tensOfMillionsToCountTo)
 }
 
-func countToNMillion(n int, m map[int]string) {
-	count := 0
+func runCpuHeavySync(tensOfMillionsToCountTo []int) {
 	watch := stopwatch.Start()
+	for _, v := range tensOfMillionsToCountTo {
+		countToNMillion(v)
+	}
+	watch.Stop()
+	fmt.Println("Synchronous function: time taken", watch.Milliseconds())
+}
+
+func countToNMillion(n int) {
+	count := 0
 	for i := 0; i < 1000000*n; i++ {
 		count += i
 	}
-	watch.Stop()
-	m[n] = watch.Milliseconds().String()
 }
