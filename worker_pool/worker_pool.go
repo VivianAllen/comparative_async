b373e129b9954faada06fee88c38e0e085e7b74f
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-10-04 15:45:45 +0100

    adds explanatory notes

--- worker_pool/worker_pool.go
+++ worker_pool/worker_pool.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	tasks := make(chan string, 10)
-	results := make(chan Result)
+	results := make(chan Result, 11)
 
 	//sets up worker pool
 	for w := 1; w <= 3; w++ {
@@ -21,10 +21,10 @@ func main() {
 	//closing the channel communicates to worker pool that no more values will be sent to urlsCh - see https://tour.golang.org/concurrency/4
 	close(tasks)
 
-	for i := 1; i <= len(urls); i++ {
-		result := <-results
-		fmt.Println(result)
+	for result := range results {
+		fmt.Println(result, len(results))
 	}
+
 }
 
 var urls = []string{"https://www.google.com", "https://www.methods.co.uk", "https://www.github.com", "https://www.stackoverflow.com", "https://go.dev", "https://www.youtube.com", "https://www.ons.gov.uk", "https://coronavirusresources.phe.gov.uk/", "https://campaignresources.phe.gov.uk/resources", "https://www.twitter.com", "https://www.facebook.com"}
@@ -37,14 +37,16 @@ type Result struct {
 }
 
 //function set up to receive from the urls channel and send to the results channel
-func workerTask(id int, urls <-chan string, results chan<- Result) {
-	for url := range urls {
+func workerTask(id int, tasks <-chan string, results chan<- Result) {
+	//each time a url from the tasks channel is used inside the workerTask function, it is removed from the tasks
+	for url := range tasks {
 		start := time.Now()
 		resp, err := http.Get(url)
 		timeElapsed := time.Since(start).Seconds()
 		if err != nil {
 			fmt.Println(err)
 		}
+		//Result is sent to results channel
 		results <- Result{workerId: id, url: url, responseCode: resp.StatusCode, speed: timeElapsed}
 	}
 }
