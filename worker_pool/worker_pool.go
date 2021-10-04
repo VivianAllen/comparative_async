46022b91c3adf87a6347a3f7f694b8bb34436188
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-10-04 16:13:20 +0100

    adds explanatory comments

--- worker_pool/worker_pool.go
+++ worker_pool/worker_pool.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
 func main() {
 	tasks := make(chan string, 10)
-	results := make(chan Result, 11)
+	results := make(chan Result)
+	//sets up wait group to track task completion
+	wg := new(sync.WaitGroup)
 
 	//sets up worker pool
 	for w := 1; w <= 3; w++ {
@@ -17,14 +20,19 @@ func main() {
 
 	for _, url := range urls {
 		tasks <- url
-	}
-	//closing the channel communicates to worker pool that no more values will be sent to urlsCh - see https://tour.golang.org/concurrency/4
-	close(tasks)
-
-	for result := range results {
-		fmt.Println(result, len(results))
+		//for each url added to the tasks channel, wait group counter is incremented by one
+		wg.Add(1)
 	}
 
+	go func(results <-chan Result) {
+		for result := range results {
+			fmt.Println(result)
+			//for each result printed, wait group counter is decremented
+			wg.Done()
+		}
+	}(results)
+	//function will wait for wait group counter to reach zero before exiting
+	wg.Wait()
 }
 
 var urls = []string{"https://www.google.com", "https://www.methods.co.uk", "https://www.github.com", "https://www.stackoverflow.com", "https://go.dev", "https://www.youtube.com", "https://www.ons.gov.uk", "https://coronavirusresources.phe.gov.uk/", "https://campaignresources.phe.gov.uk/resources", "https://www.twitter.com", "https://www.facebook.com"}
@@ -39,6 +47,7 @@ type Result struct {
 //function set up to receive from the urls channel and send to the results channel
 func workerTask(id int, tasks <-chan string, results chan<- Result) {
 	//each time a url from the tasks channel is used inside the workerTask function, it is removed from the tasks
+	//when there are no tasks remaining in the channel, workerTask goroutine will exit
 	for url := range tasks {
 		start := time.Now()
 		resp, err := http.Get(url)
