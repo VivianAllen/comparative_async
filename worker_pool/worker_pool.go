365e65336cd6254eb8e0a11589353779fd682941
Patrick Fleming <patrickjosephfleming@gmail.com>
2021-10-06 15:27:36 +0100

    formats long lines and improves naming of channel variables

--- worker_pool/worker_pool.go
+++ worker_pool/worker_pool.go
@@ -8,12 +8,25 @@ import (
 )
 
 func main() {
-	var urls = []string{"https://www.google.com", "https://www.methods.co.uk", "https://www.github.com", "https://www.stackoverflow.com", "https://go.dev", "https://www.youtube.com", "https://www.ons.gov.uk", "https://coronavirusresources.phe.gov.uk/", "https://campaignresources.phe.gov.uk/resources", "https://www.twitter.com", "https://www.facebook.com"}
-	//because the urls will be sent to the tasks channel synchronously, the channel must be sufficiently buffered to have space to receive them (I think)
-	tasks := make(chan string, 8)
-	//goroutines allow the workerPool function to return the results channel before workerTasks have completed, and before the results channel has been closed.  This means the results channel does not need to be buffered, it can print the results one by one as they are sent
-	results := make(chan Result)
-	for result := range workerPool(urls, tasks, results) {
+	var urls = []string{
+		"https://www.google.com",
+		"https://www.methods.co.uk",
+		"https://www.github.com",
+		"https://www.stackoverflow.com",
+		"https://go.dev",
+		"https://www.youtube.com",
+		"https://www.ons.gov.uk",
+		"https://coronavirusresources.phe.gov.uk/",
+		"https://campaignresources.phe.gov.uk/resources",
+		"https://www.twitter.com", "https://www.facebook.com"}
+	//because the urls will be sent to the tasks channel synchronously, the channel must be sufficiently buffered to
+	//have space to receive them (I think)
+	tasksCh := make(chan string, 8)
+	//goroutines allow the workerPool function to return the results channel before workerTasks have completed, and
+	//before the results channel has been closed.  This means the results channel does not need to be buffered, it can
+	//print the results one by one as they are sent
+	resultsCh := make(chan Result)
+	for result := range workerPool(urls, tasksCh, resultsCh) {
 		fmt.Println(result)
 	}
 }
@@ -25,33 +38,37 @@ type Result struct {
 	speed        float64
 }
 
-func workerPool(urls []string, tasks chan string, results chan Result) chan Result {
-	//wait group set up to keep track of tasks completed so that results channel can be closed and main function can return
+func workerPool(urls []string, tasksCh chan string, resultsCh chan Result) chan Result {
+	//wait group set up to keep track of tasks completed so that results channel can be closed and main function can
+	//return
 	var wg sync.WaitGroup
 
 	//sets up worker pool
 	for worker := 1; worker <= 3; worker++ {
-		//goroutine is passed pointer to waitGroup to ensure that workerPool execution is scheduled by the same waitGroup, rather than a copy
-		go workerTask(worker, tasks, results, &wg)
+		//goroutine is passed pointer to waitGroup to ensure that workerPool execution is scheduled by the same
+		//waitGroup, rather than a copy
+		go workerTask(worker, tasksCh, resultsCh, &wg)
 	}
 
 	for _, url := range urls {
-		tasks <- url
+		tasksCh <- url
 	}
 
-	//closing the results channel allows main function to return once all results have been printed (is this bad practice - not closing channel from sender function?)
+	//closing the results channel allows main function to return once all results have been printed (is this bad
+	//practice - not closing channel from sender function?)
 	go func() {
 		wg.Wait()
-		close(results)
+		close(resultsCh)
 	}()
-	return results
+	return resultsCh
 }
 
 //function set up to receive from the urls channel and send to the results channel
-func workerTask(id int, tasks <-chan string, results chan<- Result, wg *sync.WaitGroup) {
+func workerTask(id int, tasksCh <-chan string, resultsCh chan<- Result, wg *sync.WaitGroup) {
 	//each time a url from the tasks channel is used inside the workerTask function, it is removed from the tasks
-	//because the tasks channel is never closed, workerTask goroutines will wait at start of for loop, and will only exit when the workerPool function is returned
-	for url := range tasks {
+	//because the tasks channel is never closed, workerTask goroutines will wait at start of for loop, and will only
+	//exit when the workerPool function is returned
+	for url := range tasksCh {
 		//waitGroup is incremented by 1 for each task
 		wg.Add(1)
 		start := time.Now()
@@ -61,7 +78,7 @@ func workerTask(id int, tasks <-chan string, results chan<- Result, wg *sync.Wai
 			fmt.Println(err)
 		}
 		//Result is sent to results channel
-		results <- Result{workerId: id, url: url, responseCode: resp.StatusCode, speed: timeElapsed}
+		resultsCh <- Result{workerId: id, url: url, responseCode: resp.StatusCode, speed: timeElapsed}
 		//waitGroup decremented by 1 as each task is completed
 		wg.Done()
 	}
--- worker_pool/worker_pool_test.go
+++ worker_pool/worker_pool_test.go
@@ -9,8 +9,12 @@ func TestWorkerPool(t *testing.T) {
 	tasks := make(chan string, 10)
 	results := make(chan Result)
 	//because test errors cannot be raised inside a goroutine, we need to make an channel to send and receive errors
-	errs := make(chan error, 10)
-	var urls = []string{"https://www.google.com", "https://www.methods.co.uk", "https://www.github.com", "https://www.stackoverflow.com"}
+	errsCh := make(chan error, 10)
+	var urls = []string{
+		"https://www.google.com",
+		"https://www.methods.co.uk",
+		"https://www.github.com",
+		"https://www.stackoverflow.com"}
 	var expected = map[string]int{
 		"https://www.google.com":        200,
 		"https://www.methods.co.uk":     200,
@@ -23,14 +27,16 @@ func TestWorkerPool(t *testing.T) {
 			expectedResponseCode := expected[result.url]
 			if result.responseCode != expectedResponseCode {
 				//send errors to errs channel
-				errs <- fmt.Errorf("for url %v expected response code %v, got %v", result.url, expectedResponseCode, result.responseCode)
+				errsCh <- fmt.Errorf("for url %v expected response code %v, got %v",
+					result.url, expectedResponseCode, result.responseCode)
 			}
 
 		}
-		//close errs channel when results channel is closed, allowing for loop below to complete and test function to exit
-		close(errs)
+		//close errs channel when results channel is closed, allowing for loop below to complete and test function to
+		//exit
+		close(errsCh)
 	}()
-	for err := range errs {
+	for err := range errsCh {
 		if err != nil {
 			t.Fatal(err)
 		}
